2fcd9d1b9a16e3aae49ed1ea7d1d494b22cda7e8
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-11-13 17:27:04 -0300

    Ex 002

---
+++ Mundo 1/Exercícios Mundo 1/ex002.py
@@ -0,0 +1,4 @@
+# Exercício 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Informe o seu nome: ')
+print('Olá' , nome , 'Seja bem-vindo!')
