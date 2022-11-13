397220934955b6798ae38597d653e6ba76bb56d6
lucasbmiotto <lucas.b.miotto@hotmail.com>
2022-11-13 19:44:07 -0300

    Ex 003

---
+++ Mundo 1/Exercícios Mundo 1/ex003.py
@@ -0,0 +1,7 @@
+# Exercício 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Informe um número inteiro: '))
+n2 = int(input('Informe outro número inteiro: '))
+soma = n1+n2
+
+print('A soma entre {} e {} é {}'.format(n1,n2,soma))
